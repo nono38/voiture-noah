e7a5172c1b0d93040ea94003e09da85919973f60
nono38 <74985866+nono38@users.noreply.github.com>
2022-05-16 11:09:29 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,13 @@
+let noah = 0
 irRemote.connectInfrared(DigitalPin.P11)
 turtleBit.OFFLed()
 basic.forever(function () {
-    if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Ok)) {
-        turtleBit.LED_brightness(255)
-        turtleBit.SetLed(
-        LR.LeftSide,
-        255,
-        255,
-        255
-        )
-        turtleBit.SetLed(
-        LR.RightSide,
-        255,
-        255,
-        255
-        )
+    noah = 0
+    if (turtleBit.ultra() <= 18) {
+        noah = 1
+        turtleBit.state(MotorState.stop)
+        turtleBit.Motor(LR.LeftSide, MD.Forward, 50)
+        turtleBit.Motor(LR.RightSide, MD.Back, 50)
     } else if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Down)) {
         turtleBit.state(MotorState.stop)
         turtleBit.run(DIR.Run_back, 100)
@@ -31,5 +24,19 @@ basic.forever(function () {
         turtleBit.OFFLed()
     } else if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Number_2)) {
         turtleBit.state(MotorState.stop)
+    } else if (irRemote.returnIrButton() == irRemote.irButton(IrButton.Ok)) {
+        turtleBit.LED_brightness(95)
+        turtleBit.SetLed(
+        LR.LeftSide,
+        255,
+        255,
+        255
+        )
+        turtleBit.SetLed(
+        LR.RightSide,
+        255,
+        255,
+        255
+        )
     }
 })
